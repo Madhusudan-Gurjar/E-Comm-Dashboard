aa9eda2790f99bb19c2868695e8a1d212f4e975c
Madhusudan Gurjar <161649945+Madhusudan-Gurjar@users.noreply.github.com>
2025-03-23 04:54:08 +0530

    Update config.js

--- backend/db/config.js
+++ backend/db/config.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose');
 //mongoose.connect("mongodb://localhost:27017/e-commerce");
-mongoose.connect("mongodb+srv://madhusudangurjar16123:Ma@16123@cluster0.w75ca.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+mongoose.connect("mongodb+srv://madhusudangurjar16123:Ma@16123@cluster0.w75ca.mongodb.net/e-comm?retryWrites=true&w=majority&appName=Cluster0");
