ca7d68ade746343399f50e33c8af48aaa55406bb
Madhusudan Gurjar <161649945+Madhusudan-Gurjar@users.noreply.github.com>
2025-03-23 06:37:09 +0530

    Update config.js

--- backend/db/config.js
+++ backend/db/config.js
@@ -1,3 +1,12 @@
+// const mongoose = require('mongoose');
+// //mongoose.connect("mongodb://localhost:27017/e-commerce");
+// mongoose.connect("mongodb+srv://madhusudangurjar16123:Ma@16123@cluster0.w75ca.mongodb.net/e-comm?retryWrites=true&w=majority&appName=Cluster0");
+require('dotenv').config(); // Load environment variables
+
 const mongoose = require('mongoose');
-//mongoose.connect("mongodb://localhost:27017/e-commerce");
-mongoose.connect("mongodb+srv://madhusudangurjar16123:Ma@16123@cluster0.w75ca.mongodb.net/e-comm?retryWrites=true&w=majority&appName=Cluster0");
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log("✅ MongoDB Connected"))
+.catch(err => console.error("❌ MongoDB Connection Error:", err));
